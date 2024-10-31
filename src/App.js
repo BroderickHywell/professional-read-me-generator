59b3ff6a7013e5f4e465459bf6f54fd707dc269d
Broderick <zerothesu@gmail.com>
2024-10-31 15:54:05 -0600

    css change, put button and form in

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import './App.css'
 
+
+
 function App() {
   return (
     <div className="App">
       <h1>readme.md generator</h1>
-      <input type='text' placeholder='project title'/>
-      <input type='text' placeholder='description'/>
-      <input type='text' placeholder='table of contents'/>
-      <input type='text' placeholder='installation'/>
-      <input type='text' placeholder='usage'/>
-      <input type='text' placeholder='credits'/>
-      <input type='text' placeholder='license'/>
-      <input type='text' placeholder='badges'/>
-      <input type='text' placeholder='features'/>
-      <input type='text' placeholder='contribution'/>
-      <input type='text' placeholder='tests'/>
+      <span>pro tip: hit tab to quickly jump between the inputs!</span>
+      <form>
+        <input type='text' placeholder='project title(required)' required/>
+        <input type='text' placeholder='description'/>
+        <input type='text' placeholder='installation'/>
+        <input type='text' placeholder='usage'/>
+        <input type='text' placeholder='credits'/>
+        <input type='text' placeholder='license'/>
+        <input type='text' placeholder='features'/>
+        <input type='text' placeholder='contribution'/>
+        <input type='text' placeholder='tests'/>
+        <button>download your readme.md</button>
+      </form>
     </div>
   )
 }
