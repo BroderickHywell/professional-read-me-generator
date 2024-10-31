42b8f984ff4cff9d14fd992649dfb0e670beb17b
Broderick <zerothesu@gmail.com>
2024-10-31 13:46:52 -0600

    added input tags

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ function App() {
   return (
     <div className="App">
       <h1>readme.md generator</h1>
+      <input type='text' placeholder='project title'/>
+      <input type='text' placeholder='description'/>
+      <input type='text' placeholder='table of contents'/>
+      <input type='text' placeholder='installation'/>
+      <input type='text' placeholder='usage'/>
+      <input type='text' placeholder='credits'/>
+      <input type='text' placeholder='license'/>
+      <input type='text' placeholder='badges'/>
+      <input type='text' placeholder='features'/>
+      <input type='text' placeholder='contribution'/>
+      <input type='text' placeholder='tests'/>
     </div>
   )
 }
