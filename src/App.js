43cb5692ce17b8abf7c0d5cacb405a10139adb3c
Broderick <zerothesu@gmail.com>
2024-10-31 18:33:03 -0600

    added checks for each parameter the user types in

--- src/App.js
+++ src/App.js
@@ -1,21 +1,69 @@
 import './App.css'
 
-// PREVENTS THE PAGE FROM RELOADING WHEN YOU SUBMIT THE FORM
-function handleForm(event) { event.preventDefault() } 
+// prevents the page from loading when you submit the form
+let markdownContent
+function handleForm(event){ 
+  event.preventDefault() 
+} 
 
 function generateMarkdownContent() {
-  // Example content
-  const markdown = `# Heading 1
-  This is some markdown text.
+  // Creates the markdown file contents based on what the user inputed on the page
+  let markdown = `# ${document.getElementById('title-input').value}`
 
-  ## Heading 2
-  - List item 1
-  - List item 2`
+  // checks for description
+  if(document.getElementById('description-input').value !== ''){
+    markdown += `\n## Description
+    ${document.getElementById('description-input').value}`
+  }
 
+  // checks for installation
+  if(document.getElementById('installation-input').value !== ''){
+    markdown += `\n## Installation
+    ${document.getElementById('installation-input').value}`
+  }
+
+  // checks for usage
+  if(document.getElementById('usage-input').value !== ''){
+    markdown += `\n## Usage
+    ${document.getElementById('usage-input').value}`
+  }
+
+  // checks for credits
+  if(document.getElementById('credits-input').value !== ''){
+    markdown += `\n## Credits
+    ${document.getElementById('credits-input').value}`
+  }
+
+  // checks for license
+  if(document.getElementById('license-input').value !== ''){
+    markdown += `\n## License
+    ${document.getElementById('license-input').value}`
+  }
+
+  // checks for features
+  if(document.getElementById('features-input').value !== ''){
+    markdown += `\n## Features
+    ${document.getElementById('features-input').value}`
+  }
+
+  // checks for contribution
+  if(document.getElementById('contribution-input').value !== ''){
+    markdown += `\n## Contribution
+    ${document.getElementById('contribution-input').value}`
+  }
+
+  // checks for tests
+  if(document.getElementById('tests-input').value !== ''){
+    markdown += `\n## Tests
+    ${document.getElementById('tests-input').value}`
+  }
+
+  console.log(markdown)
   return markdown
 }
 
 function downloadMarkdownFile(markdownContent) {
+  markdownContent = generateMarkdownContent()
   if(document.getElementById('title-input').value !== ''){
     document.getElementById('tip-span').innerHTML = 'Happy Coding!'
     const blob = new Blob([markdownContent], { type: "text/markdown" })
@@ -23,8 +71,7 @@ function downloadMarkdownFile(markdownContent) {
   
     const link = document.createElement("a")
     link.href = url
-    link.download = "generated.md"
-    link.textContent = "Download Markdown File"
+    link.download = "README.md"
   
     document.body.appendChild(link)
     link.click()
@@ -34,7 +81,6 @@ function downloadMarkdownFile(markdownContent) {
     document.getElementById('tip-span').innerHTML = 'You at least gotta have a title to generate a markdown file.'
   }
 }
-const markdownContent = generateMarkdownContent()
 
 function buttonStatus(){
   console.log('buttonstatus')
@@ -50,14 +96,14 @@ function App() {
       <span id='tip-span'>pro tip: Hit tab to quickly jump between the inputs!</span>
       <form id='readme-form' onSubmit={handleForm}>
         <input type='text' onChange={buttonStatus} id='title-input' placeholder='project title(required)' required/>
-        <input type='text' placeholder='description'/>
-        <input type='text' placeholder='installation'/>
-        <input type='text' placeholder='usage'/>
-        <input type='text' placeholder='credits'/>
-        <input type='text' placeholder='license'/>
-        <input type='text' placeholder='features'/>
-        <input type='text' placeholder='contribution'/>
-        <input type='text' placeholder='tests'/>
+        <input type='text' id='description-input' placeholder='description'/>
+        <input type='text' id='installation-input' placeholder='installation'/>
+        <input type='text' id='usage-input' placeholder='usage'/>
+        <input type='text' id='credits-input' placeholder='credits'/>
+        <input type='text' id='license-input' placeholder='license'/>
+        <input type='text' id='features-input' placeholder='features'/>
+        <input type='text' id='contribution-input' placeholder='contribution'/>
+        <input type='text' id='tests-input' placeholder='tests'/>
         <button id='download-button' onClick={() => {downloadMarkdownFile(markdownContent)}}>download your readme.md</button>
       </form>
     </div>
