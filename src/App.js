2062847fe20ef4dc4b5f87fc2e9e1e23d00bc2c6
Broderick <zerothesu@gmail.com>
2024-11-01 16:10:45 -0600

    cleaned up code that fetches what the user inputed, removed console logs, added some css to span, added comments describing code functionality

--- src/App.js
+++ src/App.js
@@ -1,91 +1,63 @@
 import './App.css'
 
 // prevents the page from loading when you submit the form
-let markdownContent
-function handleForm(event){ 
-  event.preventDefault() 
-} 
+let markdownContent // defines markdown content to be used later by multiple functions
+function handleForm(event){ event.preventDefault() } 
 
-function generateMarkdownContent() {
-  // Creates the markdown file contents based on what the user inputed on the page
+// function for getting data for 
+function fetchUserInputData() {
+  // creates markdown file based on what user inputed on page
   let markdown = `# ${document.getElementById('title-input').value}`
+  
+  // checks which parameters the user filled and adds them to the markdown data
+  if(document.getElementById('description-input').value !== ''){markdown += `\n## Description \n${document.getElementById('description-input').value}`}
+  if(document.getElementById('installation-input').value !== ''){markdown += `\n## Installation \n${document.getElementById('installation-input').value}`}
+  if(document.getElementById('usage-input').value !== ''){markdown += `\n## Usage \n${document.getElementById('usage-input').value}`}
+  if(document.getElementById('credits-input').value !== ''){markdown += `\n## Credits \n${document.getElementById('credits-input').value}`}
+  if(document.getElementById('license-input').value !== ''){markdown += `\n## License \n${document.getElementById('license-input').value}`}
+  if(document.getElementById('features-input').value !== ''){markdown += `\n## Features \n${document.getElementById('features-input').value}`}
+  if(document.getElementById('contribution-input').value !== ''){markdown += `\n## Contribution \n${document.getElementById('contribution-input').value}`}
+  if(document.getElementById('tests-input').value !== ''){markdown += `\n## Tests \n${document.getElementById('tests-input').value}`}
 
-  // checks for description
-  if(document.getElementById('description-input').value !== ''){
-    markdown += `\n## Description \n${document.getElementById('description-input').value}`
-  }
-
-  // checks for installation
-  if(document.getElementById('installation-input').value !== ''){
-    markdown += `\n## Installation \n${document.getElementById('installation-input').value}`
-  }
-
-  // checks for usage
-  if(document.getElementById('usage-input').value !== ''){
-    markdown += `\n## Usage \n${document.getElementById('usage-input').value}`
-  }
-
-  // checks for credits
-  if(document.getElementById('credits-input').value !== ''){
-    markdown += `\n## Credits \n${document.getElementById('credits-input').value}`
-  }
-
-  // checks for license
-  if(document.getElementById('license-input').value !== ''){
-    markdown += `\n## License \n${document.getElementById('license-input').value}`
-  }
-
-  // checks for features
-  if(document.getElementById('features-input').value !== ''){
-    markdown += `\n## Features \n${document.getElementById('features-input').value}`
-  }
-
-  // checks for contribution
-  if(document.getElementById('contribution-input').value !== ''){
-    markdown += `\n## Contribution \n${document.getElementById('contribution-input').value}`
-  }
-
-  // checks for tests
-  if(document.getElementById('tests-input').value !== ''){
-    markdown += `\n## Tests \n${document.getElementById('tests-input').value}`
-  }
-
-  console.log(markdown)
   return markdown
 }
 
 function downloadMarkdownFile(markdownContent) {
-  markdownContent = generateMarkdownContent()
+  markdownContent = fetchUserInputData()
+
+  // checks for project title which is the bare minimum to create markdown file
   if(document.getElementById('title-input').value !== ''){
     document.getElementById('tip-span').innerHTML = 'Happy Coding!'
+
+    // code creates markdown link, autoselects it then removes it so the download can start right as the download button is pressed
     const blob = new Blob([markdownContent], { type: "text/markdown" })
     const url = URL.createObjectURL(blob)
-  
     const link = document.createElement("a")
     link.href = url
     link.download = "README.md"
-  
     document.body.appendChild(link)
     link.click()
     document.body.removeChild(link)
     URL.revokeObjectURL(url)
   }else{
+    // code for when the project title input is empty and you submit the form
     document.getElementById('tip-span').innerHTML = 'You at least gotta have a title to generate a markdown file.'
   }
 }
 
+// sets the display of the download button so you can't press it if there is no project title
 function buttonStatus(){
-  console.log('buttonstatus')
   document.getElementById('title-input').value === '' ?
     document.getElementById('download-button').style.display = 'none' :
     document.getElementById('download-button').style.display = 'flex'
 }
 
+// main function
 function App() {
   return (
     <div className="App">
       <h1>readme.md generator</h1>
-      <span id='tip-span'>tip: Hit tab to quickly jump between the inputs! Also, typing \n will create a new line</span>
+      <span id='tip-span'>tip: Hit tab to quickly jump between the inputs! Also, typing \n will create a new line in the markdown document!</span>
       <form id='readme-form' onSubmit={handleForm}>
         <input type='text' onChange={buttonStatus} id='title-input' placeholder='project title(required)' required/>
         <input type='text' id='description-input' placeholder='description'/>
